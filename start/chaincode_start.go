a6ef90ccf6fc10bd9c1011548e1c5365c31c58ff
System Administrator <root@MacBook-Pro-de-Oriol.local>
2017-03-18 21:16:51 +0100

    First version - Create voting & Query

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -19,6 +19,10 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"encoding/json"
+	"time"
+//	"strings"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -27,53 +31,227 @@ import (
 type SimpleChaincode struct {
 }
 
+var votingIndexStr = "_votingindex"
+
+type Member struct{
+	Id string `json:"id"`				
+	Name string `json:"name"`
+	Category string `json:"category"`
+	Office string `json:"office"`
+}
+
+type Vote struct{
+	Voter Member `json:"voter"`
+	Justification string `json:"justification"`
+}
+
+type Option struct{
+	Id int `json:"id"`
+	Description string `json:"description"`
+	NumberOfVotes int `json:"number_of_votes"`
+	Votes []Vote `json:"votes"`
+}
+
+type Voting struct{
+	Id string `json:"id"`
+	Description string `json:"description"`
+	Voters []Member `json:"voters"`
+	Options []Option `json:"options"`
+	Status bool `json:"status"`
+	VotingDeadlineInMinutes int `json:"voting_deadline_in_minutes"`
+	StartVotingTimestamp int64 `json:"start_voting_timestamp"` 
+}
+
+type AllVotings struct{
+	Votings []Voting `json:"votings"`
+}
+
 // ============================================================================================================================
 // Main
 // ============================================================================================================================
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("Error starting Voting chaincode: %s", err)
 	}
 }
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	var Aval int
+	var err error
+	var jsonAsBytes []byte 	
+
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
-        fmt.Println("Chaincode de prueba")
-	 err := stub.PutState("hello_world", []byte(args[0]))
-    	if err != nil {
-        	return nil, err
-    	}
 
+
+	// Initialize the chaincode
+	Aval, err = strconv.Atoi(args[0])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for asset holding")
+	}
+
+	// Write the state to the ledger
+	err = stub.PutState("abc", []byte(strconv.Itoa(Aval)))				//making a test var "abc", I find it handy to read/write to it right away to test the network
+	if err != nil {
+		return nil, err
+	}
+	
+	var votings AllVotings
+	jsonAsBytes, _ = json.Marshal(votings)
+	err = stub.PutState(votingIndexStr, jsonAsBytes)
+	if err != nil {
+		return nil, err
+	}
+	
 	return nil, nil
+
 }
 
 // Invoke is our entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
 	fmt.Println("invoke is running " + function)
 
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "add_voting" {										//deletes an entity from its state
+		res, err := t.add_voting(stub, args)
+		return res, err
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
-	return nil, errors.New("Received unknown function invocation: " + function)
+	return nil, errors.New("Received unknown function invocation")
+
+}
+
+func (t *SimpleChaincode) add_voting(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+	var option Option
+	
+	//	0        1      2     3      4      5       6
+	//["1", "Iniciativas Innovacion 2017", "30", "", "16"] *"blue", "35*
+	if len(args) < 4 {
+		return nil, errors.New("Incorrect number of arguments. Expecting like 5?")
+	}
+
+	// 1st argument - Voting ID
+	id1, err := strconv.Atoi(args[0])
+	if err != nil {
+		return nil, errors.New("1st argument must be a numeric string")
+	}
+
+	// 2nd argument - Voting Description
+	if len(args[1]) <= 0 {
+                return nil, errors.New("2nd argument must be a non-empty string")
+        }
+
+	// 3rd argument - VotingDeadlineInMinutes 
+        deadline1, err := strconv.Atoi(args[2])
+        if err != nil {
+                return nil, errors.New("3rd argument must be a numeric string")
+        }
+
+	fmt.Println("Creating new Voting...")
+	voting := Voting{}
+	voting.Id = strconv.Itoa(id1)
+	fmt.Println("voting.Id: %s", voting.Id)
+	voting.Description = args[1]
+        fmt.Println("voting.Description: %s", voting.Description)
+	voting.StartVotingTimestamp = makeTimestamp()
+	fmt.Println("voting.StartVotingTimestamp: %s", strconv.FormatInt(voting.StartVotingTimestamp, 10))	
+	voting.VotingDeadlineInMinutes = deadline1
+	fmt.Println("voting.VotingDeadlineInMinutes: %s", strconv.Itoa(voting.VotingDeadlineInMinutes))	
+
+	// voting.Voters
+	jsonAsBytes, _ := json.Marshal(voting)
+	
+	fmt.Println("Add Voting - PutState")
+	err = stub.PutState("_debug1", jsonAsBytes)
+	
+	optionId := 1	
+	for i:=2; i < len(args); i++ {
+		
+		fmt.Println("Creating new voting Option...")
+		fmt.Println("Option.Id: " + strconv.Itoa(optionId))
+		option.Id = optionId
+		optionId++;
+		
+		if len(args[i]) <= 0 {
+			return nil, errors.New(strconv.Itoa(i) + " argument must be a non-empty string")
+		}
+		option.Description = args[i] 		
+		fmt.Println("Option.Description: %s", option.Description)		
+
+		option.NumberOfVotes = 0		
+		fmt.Println("Option.NumberOfVotes: %s", strconv.Itoa(option.NumberOfVotes)) 
+		
+		// TODO: Pendent inicialitzar vots
+		
+		fmt.Println("! created option: " + strconv.Itoa(option.Id))
+		jsonAsBytes, _ = json.Marshal(option)
+		err = stub.PutState("_debug2", jsonAsBytes)
+		
+		voting.Options = append(voting.Options, option)
+		fmt.Println("! appended option to voting")
+		i++;
+	}
+	
+	//get the voting struct
+	votingsAsBytes, err := stub.GetState(votingIndexStr)
+	if err != nil {
+		return nil, errors.New("Failed to get voting")
+	}
+	var allvotings AllVotings
+	json.Unmarshal(votingsAsBytes, &allvotings)										//un stringify it aka JSON.parse()
+	
+	allvotings.Votings = append(allvotings.Votings, voting);				
+	fmt.Println("! appended voting to allvotings")
+	jsonAsBytes, _ = json.Marshal(allvotings)
+	err = stub.PutState(votingIndexStr, jsonAsBytes)					
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("- end init voting")
+	return nil, nil
+}
+
+func makeTimestamp() int64 {
+    return time.Now().UnixNano() / (int64(time.Millisecond)/int64(time.Nanosecond))
 }
 
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
+	if function == "read" {													//read a variable
+		return t.read(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)						//error
 
-	return nil, errors.New("Received unknown function query: " + function)
+	return nil, errors.New("Received unknown function query")
+
+}
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
+	}
+
+	name = args[0]
+	valAsbytes, err := stub.GetState(name)									//get the var from chaincode state
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil													//send it onward
 }
